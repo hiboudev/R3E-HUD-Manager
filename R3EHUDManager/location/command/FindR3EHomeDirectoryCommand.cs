324ddd1542c1e0064488f4acfd146f9b1371c57e
Damien Laurent <hiboudev@gmail.com>
2017-12-06 19:35:44 +0100

    Give a chance to change custom R3E directory when app can't find it, in case it's invalid.

--- R3EHUDManager/location/command/FindR3EHomeDirectoryCommand.cs
+++ R3EHUDManager/location/command/FindR3EHomeDirectoryCommand.cs
@@ -31,21 +31,34 @@ namespace R3EHUDManager.location.command
             if (R3eHomePath != null)
             {
                 locationModel.R3eHomeBaseDirectory = R3eHomePath;
+                return;
+            }
+
+
+            string txtFile = Path.Combine(locationModel.AppDataDirectory, "customR3eDirectory.txt");
+
+            if (File.Exists(txtFile))
+            {
+                if(MessageBox.Show("Use your custom R3E directory?", "Use custom directory?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    string r3eDirectory = File.ReadAllText(txtFile);
+                    locationModel.R3eHomeBaseDirectory = r3eDirectory;
+                    return;
+                }
+            }
+
+            var dialog = new FolderBrowserDialog();
+            dialog.Description = @"Please select your 'RaceRoom Racing Experience' directory (Documents\My Games\SimBin\RaceRoom Racing Experience). Its name may end with 'Install N'.";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                locationModel.R3eHomeBaseDirectory = dialog.SelectedPath;
+                File.WriteAllText(txtFile, locationModel.R3eHomeBaseDirectory);
             }
             else
             {
-                var dialog = new FolderBrowserDialog();
-                dialog.Description = @"Please select your 'RaceRoom Racing Experience' directory (Documents\My Games\SimBin\RaceRoom Racing Experience). Its name may end with 'Install N'.";
-
-                if (dialog.ShowDialog() == DialogResult.OK)
-                {
-                    locationModel.R3eHomeBaseDirectory = dialog.SelectedPath;
-                }
-                else
-                {
-                    MessageBox.Show("Can't find R3E user directory, application will exit.", "Error");
-                    Environment.Exit(0);
-                }
+                MessageBox.Show("Can't find R3E user directory, application will exit.", "Error");
+                Environment.Exit(0);
             }
         }
     }
