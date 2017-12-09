01874471c97601fa2f1ff84b2a124e4aa820323d
Damien Laurent <hiboudev@gmail.com>
2017-12-09 03:21:41 +0100

    Changed how the background preview draws, no more flickering for crop zones.

--- R3EHUDManager/background/view/BackgroundPreviewView.cs
+++ R3EHUDManager/background/view/BackgroundPreviewView.cs
@@ -12,10 +12,13 @@ namespace R3EHUDManager.background.view
 {
     class BackgroundPreviewView : Panel
     {
+        private float internalScale;
         private Bitmap bitmap;
         private Size bitmapSize;
         private float rectangleRightRatio = 0;
         private float rectangleXRatio;
+        private Size originalBitmapSize;
+        private const int MAX_BITMAP_SIZE = 1280;
 
         public BackgroundPreviewView()
         {
@@ -26,7 +29,27 @@ namespace R3EHUDManager.background.view
 
         internal void SetBitmap(Bitmap bitmap)
         {
-            this.bitmap = bitmap;
+            originalBitmapSize = new Size(bitmap.Width, bitmap.Height);
+
+            Size newSize = new Size();
+            float bitmapRatio = bitmap.PhysicalDimension.Width / bitmap.PhysicalDimension.Height;
+
+            if(bitmapRatio > 1)
+            {
+                newSize.Width = MAX_BITMAP_SIZE;
+                newSize.Height = (int)(MAX_BITMAP_SIZE / bitmapRatio);
+            } 
+            else
+            {
+                newSize.Height = MAX_BITMAP_SIZE;
+                newSize.Width = (int)(MAX_BITMAP_SIZE * bitmapRatio);
+            }
+
+            internalScale = (float)newSize.Width / originalBitmapSize.Width;
+
+            this.bitmap = new Bitmap(bitmap, newSize);
+            bitmap.Dispose();
+
             RefreshBackground();
         }
 
@@ -55,8 +78,8 @@ namespace R3EHUDManager.background.view
         {
             if (bitmap == null) return;
 
-            rectangleRightRatio = (x+width) / bitmap.PhysicalDimension.Width;
-            rectangleXRatio = x / bitmap.PhysicalDimension.Width;
+            rectangleXRatio = (float)x / originalBitmapSize.Width;
+            rectangleRightRatio = (float)(x + width) / originalBitmapSize.Width;
 
             Invalidate();
         }
@@ -71,47 +94,42 @@ namespace R3EHUDManager.background.view
         {
             base.OnPaint(e);
 
-            if (bitmap == null) return;
+            if (bitmap == null || bitmapSize == null) return;
 
             int marginX = (Width - bitmapSize.Width) / 2;
             int marginY = (Height - bitmapSize.Height) / 2;
 
-            e.Graphics.DrawImage(bitmap, new Rectangle(marginX, marginY, bitmapSize.Width, bitmapSize.Height));
-
-
-            if (rectangleRightRatio <= 0 || bitmapSize == null)
+            if (rectangleRightRatio <= 0)
+            {
+                e.Graphics.DrawImage(bitmap, new Rectangle(marginX, marginY, bitmapSize.Width, bitmapSize.Height));
                 return;
+            }
 
-            int areaLeft = (int)Math.Round(rectangleXRatio * bitmapSize.Width);
-            int areaRight = (int)Math.Round(rectangleRightRatio * bitmapSize.Width);
+            Bitmap paintBitmap = new Bitmap(bitmap);
+            Graphics paintSurface = Graphics.FromImage(paintBitmap);
 
-            e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(140, Color.Black)), 
+            int areaLeft = (int)Math.Round(rectangleXRatio * bitmap.Width);
+            int areaRight = (int)Math.Round(rectangleRightRatio * bitmap.Width);
+
+            paintSurface.FillRectangle(new SolidBrush(Color.FromArgb(140, Color.Black)),
                 new Rectangle(
-                    marginX,
-                    marginY,
+                    0,
+                    0,
                     areaLeft,
-                    bitmapSize.Height));
+                    paintBitmap.Height));
 
-            e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(140, Color.Black)),
+            paintSurface.FillRectangle(new SolidBrush(Color.FromArgb(140, Color.Black)),
                 new Rectangle(
-                    areaRight + marginX,
-                    marginY,
-                    bitmapSize.Width - areaRight,
-                    bitmapSize.Height));
+                    areaRight,
+                    0,
+                    paintBitmap.Width - areaRight,
+                    paintBitmap.Height));
 
-            e.Graphics.DrawLine(new Pen(Color.Black, 2)
-            {
-                Alignment = PenAlignment.Center,
-            },
-                new Point(areaLeft + marginX, marginY),
-                new Point(areaLeft + marginX, marginY + bitmapSize.Height));
 
-            e.Graphics.DrawLine(new Pen(Color.Black, 2)
-            {
-                Alignment = PenAlignment.Center,
-            },
-                new Point(areaRight + marginX, marginY),
-                new Point(areaRight + marginX, marginY + bitmapSize.Height));
+            e.Graphics.DrawImage(paintBitmap, new Rectangle(marginX, marginY, bitmapSize.Width, bitmapSize.Height));
+
+            paintSurface.Dispose();
+            paintBitmap.Dispose();
         }
 
         private void OnDispose(object sender, EventArgs e)
