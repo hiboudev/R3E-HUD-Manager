84db3cc4de1d1a530561183fa8ed0645595d4d3e
Damien Laurent <hiboudev@gmail.com>
2017-12-10 13:35:05 +0100

    When switching to single screen, ask user to reset outside placeholders position to center screen.

--- R3EHUDManager/screen/command/ChangeScreenLayoutCommand.cs
+++ R3EHUDManager/screen/command/ChangeScreenLayoutCommand.cs
@@ -1,6 +1,8 @@
 ﻿using da2mvc.command;
 using R3EHUDManager.application.events;
 using R3EHUDManager.contextmenu.events;
+using R3EHUDManager.coordinates;
+using R3EHUDManager.placeholder.model;
 using R3EHUDManager.screen.events;
 using R3EHUDManager.screen.model;
 using System;
@@ -8,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace R3EHUDManager.screen.command
 {
@@ -15,16 +18,58 @@ namespace R3EHUDManager.screen.command
     {
         private readonly ContextMenuEventArgs args;
         private readonly ScreenModel screenModel;
+        private readonly PlaceHolderCollectionModel collectionModel;
 
-        public ChangeScreenLayoutCommand(ContextMenuEventArgs args, ScreenModel screenModel)
+        public ChangeScreenLayoutCommand(ContextMenuEventArgs args, ScreenModel screenModel, PlaceHolderCollectionModel collectionModel)
         {
             this.args = args;
             this.screenModel = screenModel;
+            this.collectionModel = collectionModel;
         }
 
         public void Execute()
         {
-            screenModel.SetLayout((ScreenLayoutType)args.ItemId);
+            ScreenLayoutType layout = (ScreenLayoutType)args.ItemId;
+            
+            if (screenModel.Layout == layout) return;
+
+            if (layout == ScreenLayoutType.SINGLE)
+                CheckOutsidePlaceholder();
+            
+            screenModel.SetLayout(layout);
+        }
+
+        private void CheckOutsidePlaceholder()
+        {
+            bool outsidePlaceholder = false;
+
+            foreach (PlaceholderModel placeholder in collectionModel.Placeholders)
+            {
+                ScreenPositionType screen = ScreenUtils.GetScreen(placeholder);
+                if (screen != ScreenPositionType.CENTER)
+                {
+                    outsidePlaceholder = true;
+                    break;
+                }
+            }
+
+            if (outsidePlaceholder)
+            {
+                DialogResult result = MessageBox.Show("Reset placeholders position to center screen?", "Placeholders outside center screen", MessageBoxButtons.YesNo);
+                if(result == DialogResult.Yes)
+                {
+                    foreach (PlaceholderModel placeholder in collectionModel.Placeholders)
+                    {
+                        ScreenPositionType screen = ScreenUtils.GetScreen(placeholder);
+                        if (screen != ScreenPositionType.CENTER)
+                        {
+                            R3ePoint offset = ScreenUtils.ToScreenOffset(placeholder, ScreenPositionType.CENTER);
+                            R3ePoint newPosition = new R3ePoint(placeholder.Position.X + offset.X, placeholder.Position.Y + offset.Y);
+                            collectionModel.UpdatePlaceholderPosition(placeholder.Name, newPosition);
+                        }
+                    }
+                }
+            }
         }
     }
 }
